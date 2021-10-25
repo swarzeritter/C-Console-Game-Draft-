07faad12c3228c2e05eac28f470b0957096bd3b5
swarzeritter <swarzeritter4ever@gmail.com>
2021-10-26 00:44:47 +0300

    upd

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 
 /*
 Will do
--Stats
 -Inv system
 -item buffs
 -and a lot more
@@ -12,12 +11,12 @@ Will do
 
 #define logotype			\
 "||============\n\
-|| DUELIST alpha 0.2\n\
+|| DUELIST alpha 0.3\n\
 ||\n\
 |=> "
 using namespace std;
 
-bool tutend = true;
+bool auth = false;
 int bot_try;
 char bot_do;
 ofstream save;
@@ -26,8 +25,23 @@ string cmd;
 
 enum choice {
 	duel,
-	inv
+	inv,
+	stats
 };
+
+void Dialog(string text) {
+	cout << text << endl;
+	cout << "\n\nENTER - щоб продовжити" << endl;
+	getchar();
+	system("cls");
+}
+
+void tutorial() {
+	Dialog("Привiт гравцю!");
+	Dialog("Як ти з назви зрозумiв, ти будешь грати за вiдчайдушного дуелянта, котрий намагатиметься одолати всiх своїх опонентiв!");
+	Dialog("Ти напевно спитаєш:\n- А навiщо менi це?");
+	Dialog("А я вiдповiм:\n- А хто його знае, бо ця мiнi-гра є бонусним завданням для вступу до спецiальностi :)");
+}
 
 class Player {
 public:
@@ -43,13 +57,18 @@ public:
 	int duel_num = 0;
 };
 
+
 void OnGameInit(int command) {
 	Player player, bot;
 	ifstream check("save.txt");
 	if (!check.is_open()) {
 		check.close();
 		//
-		cout << "Creating new profile...\n";
+		tutorial();
+		cout << "[Creating new profile...]\n";
+		cout << "Привiт юний лицарю, назови своє iм'я: " << endl;
+		cin >> player.name;
+		system("cls");
 		save.open("save.txt", ios_base::out);
 		save << player.name << endl \
 			<< player.exp << endl\
@@ -166,7 +185,7 @@ void OnGameInit(int command) {
 					cout << "Ви були готовi вiдбити атаку, але ваш супротивник забув вимкнути праску\n[HP] -0 HP у обох гравцiв!" << endl;
 				}
 				if (duel_cmd == 'd' && player.do_chance == false && bot_do == 'a' && bot.do_chance == false) {
-					cout << "При нападi супротивника, ви раптово втратили рiвновагу, але боту щойно позвонили по телфону, тому вiн змушений був вiдповiсти на нього\n[HP] -0 HP у обох гравцiв!" << endl;
+					cout << "При нападi супротивника, ви раптово втратили рiвновагу, але боту щойно позвонили по телфону, \nтому вiн змушений був вiдповiсти на нього\n[HP] -0 HP у обох гравцiв!" << endl;
 				}
 				//cout << "debug: " << duel_cmd << " " << bot_do << " " << player.do_chance << " " << bot.do_chance << endl;
 				break;
@@ -184,9 +203,22 @@ void OnGameInit(int command) {
 		player.duel_num++;
 		break;
 		
-	case inv:
-		cout << "|Тут колись з'явиться iнвентар!\n\n";
+	case stats:
+		system("cls");
+		cout << "=======[Ваш iгровий персонаж]========" << endl\
+			<< "Iм'я: " << player.name << endl \
+			<< "Досвiд: " << player.exp << endl\
+			<< "Рiвень: " << player.lvl << endl\
+			<< "Урон: " << player.damage << endl\
+			<< "Шанс попадання: " << player.hit << endl\
+			<< "Шанс захисту: " << player.protection << endl\
+			<< "Шанс парування" << player.parry << endl\
+			<< "Кi-сть дуелей: " << player.duel_num << endl;
+		cout << "\n\nback - щоб повернутися\n" << endl;
+		cin >> cmd;
 		break;
+	case inv:
+		cout << "work in progress" << endl;
 	default:;
 	}
 	//autosave
@@ -197,24 +229,42 @@ void OnGameInit(int command) {
 		<< player.damage << endl\
 		<< player.hit << endl\
 		<< player.protection << endl\
-		<< player.parry << endl;
+		<< player.parry << endl\
+		<< player.duel_num << endl;
 	save.close();
 }
 
-void Dialog(string text) {
-	cout << text << endl;
-	cout << "\n\nENTER - щоб продовжити" << endl; 
-	getchar(); 
-	system("cls");
+void OnGameLoad() {
+	cout << "|+-/|==>Save System\n\
+|Введiть:\n\
+|1 - для створення нового профiлю" << endl;
+	ifstream check("save.txt");
+	cout << ((!check.is_open()) ? "" : "|2 - для завантаження вже iснуючого профiля") << endl;
+	cin >> cmd;
+	if (cmd == "1") {
+		if (check.is_open()) {
+			cout << "|Система знайшла данi про iснуючий профiль, знищити i створити новий(y/n)?" << endl;
+			cin >> cmd;
+			if (cmd == "y") {
+				check.close();
+				remove("save.txt");
+				OnGameInit(666);
+			}
+			else {
+				system("cls");
+				return OnGameLoad();
+			}
+		}
+		else {
+			OnGameInit(666);
+		}
+	}
+	else if (cmd == "2" && (check.is_open())) {
+		OnGameInit(666);
+	}
+	check.close();
+	auth = true;
 }
-
-void tutorial() {
-	Dialog("Привiт гравцю!");
-	Dialog("Як ти з назви зрозумiв, ти будешь грати за вiдчайдушного дуелянта, котрий намагатиметься одолати всiх своїх опонентiв!");
-	Dialog("Ти напевно спитаєш:\n- А навiщо менi це?");
-	Dialog("А я вiдповiм:\n- А хто його знае, бо ця мiнi-гра є бонусним завданням для вступу до спецiальностi :)");
-}
-
 
 void cmds(string cmd) {
 	if (cmd == "help"){
@@ -230,17 +280,21 @@ void cmds(string cmd) {
 	else if (cmd == "inv") {
 		OnGameInit(inv);
 	}
+	else if (cmd == "stats") {
+		OnGameInit(stats);
+	}
 	else if (cmd == "q" or cmd == "quit") {
 		exit(0);
 	}
 	else {
-		cout << "|Помилка, invalid command! Try to write /help\n" << endl;
+		cout << "|Помилка, invalid command! Try to write help\n" << endl;
 	}
 }
 
 int main() {
 	setlocale(LC_ALL, "rus");
-	if (!tutend) { tutorial(); }
+	//OnGameInit(666);
+	if (!auth) { OnGameLoad(); }
 	cout << logotype;
 	cin >> cmd;
 	cmds(cmd);
